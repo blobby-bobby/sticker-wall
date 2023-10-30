ce21b38fcc9a3421426fca900d138172829a4c91
blobby-bobby <bulle.ouvrard@gmail.com>
2023-10-30 15:11:47 +0100

    fix updates data

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -52,18 +52,18 @@ export interface UpdatesType {
 // Updates data
 export const updatesData: UpdatesType[] = [
     {
+        day: "27/09/2023",
+        image: 'cheese.png',
+        title: "Lancement du Sticker Wall",
+        description: "Et voilà, le Sticker Wall est en ligne. Pour cette première version, la fonctionnalité '1 clic, 1 sticker sur le mur' est là, et les bases ont été posées pour faire évoluer ce Sticker Wall =). Cette version de départ comporte un kit de départ de 8 stickers qui devrait s'étoffer dans le temps.",
+        next: ["plus de stickers", "pourquoi pas ajouter un panneau qui permettra de choisir le sticker à coller sur le mur", "personnaliser le curseur"],
+    },
+    {
         day: "10/10/2023",
         image: 'isaac.png',
         title: "Nouvelles features",
         description: "Parmi les améliorations intégrées, la possibilité de nettoyer le mur en enlevant tous les stickers. Ainsi que la fonctionnalité SCREENSHOT, pour transformer son mur en fond d'écran. Merci Redux =) ! Et au passage, j'ai profité de l'ajout de modales pour passer un coup de poliche sur les interfaces, et pour concocter un design system plus poussé, avec un UI kit fait maison.",
         next: ["encore plus de stickers", "des stickers animés", "personnaliser le curseur", "plus d'aléatoire dans les stickers"],
-    },
-    {
-      day: "27/09/2023",
-	  image: 'cheese.png',
-      title: "Lancement du Sticker Wall",
-      description: "Et voilà, le Sticker Wall est en ligne. Pour cette première version, la fonctionnalité '1 clic, 1 sticker sur le mur' est là, et les bases ont été posées pour faire évoluer ce Sticker Wall =). Cette version de départ comporte un kit de départ de 8 stickers qui devrait s'étoffer dans le temps.",
-      next: ["plus de stickers", "pourquoi pas ajouter un panneau qui permettra de choisir le sticker à coller sur le mur", "personnaliser le curseur"],
     },
     {
         day: "30/10/2023",
